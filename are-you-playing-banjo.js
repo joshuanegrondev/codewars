3977a296c37c7e2efb2178f13bc6f6f72aec416a
Joshua Negron <69063454+joshuanegrondev@users.noreply.github.com>
2020-10-20 23:02:33 -0400

    Add files via upload

---
+++ are-you-playing-banjo.js
@@ -0,0 +1,24 @@
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+//
+// The function takes a name as its only argument, and returns one of the following strings:
+//
+// name + " plays banjo"
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+
+function areYouPlayingBanjo(name) {
+return (name[0] === 'R' ||name[0] === 'r' ) ? name + ' plays banjo' : name + ' does not play banjo';
+}
+
+// function areYouPlayingBanjo(name) {
+// if(name[0] === 'R' ||name[0] === 'r' ){
+//   return name + ' plays banjo'
+// } else {
+//   return name + ' does not play banjo'
+// }
+// }
+
+// Test.assertEquals(areYouPlayingBanjo("Martin"), "Martin does not play banjo");
+// Test.assertEquals(areYouPlayingBanjo("Rikke"), "Rikke plays banjo");
